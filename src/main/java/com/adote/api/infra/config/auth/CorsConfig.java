45c27ad4c25768736a66c352288e43ab5775abe5
Felipe Wai <felipe.wai@t2s.com.br>
2025-02-25 14:43:55 -0300

    fix: cors2.0

--- src/main/java/com/adote/api/infra/config/auth/CorsConfig.java
+++ src/main/java/com/adote/api/infra/config/auth/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://seusite.com")); // Altere conforme necessário
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
