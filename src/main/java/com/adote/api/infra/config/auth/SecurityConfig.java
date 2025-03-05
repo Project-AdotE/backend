d4eafd3f321e435e82a64375c5bb22315a87f5e7
Felipe Wai <felipe.wai@t2s.com.br>
2025-03-05 10:32:24 -0300

    fix: patch cors error

--- src/main/java/com/adote/api/infra/config/auth/SecurityConfig.java
+++ src/main/java/com/adote/api/infra/config/auth/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://adot-e.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://adot-e.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin"));
         configuration.setAllowCredentials(true);
