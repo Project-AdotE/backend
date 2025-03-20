e702a0ece4f5a8be0ba861d99f9b01ea69e0e6a0
Felipe Wai <felipe.wai@t2s.com.br>
2025-03-20 15:40:47 -0300

    fix: user agent qrcode http request

--- src/main/java/com/adote/api/infra/service/QrCodePixService.java
+++ src/main/java/com/adote/api/infra/service/QrCodePixService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
