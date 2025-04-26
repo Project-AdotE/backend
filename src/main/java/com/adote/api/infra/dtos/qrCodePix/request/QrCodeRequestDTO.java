7714178d365c6a96e20b9a55aee6a60c7058e6ae
Felipe Wai <felipe.wai@t2s.com.br>
2025-04-26 01:22:00 -0300

    fix: excluido DTO para qrCode

--- src/main/java/com/adote/api/infra/dtos/qrCodePix/request/QrCodeRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.adote.api.infra.dtos.qrCodePix.request;
-
-public record QrCodeRequestDTO(
-        String tipo,
-        String chave,
-        String nome,
-        String cidade
-) {
-}
