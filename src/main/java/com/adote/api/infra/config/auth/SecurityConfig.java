2d7713c50724e74c1b0029eebe1db008b29de2f7
Felipe Wai <felipe.wai@t2s.com.br>
2025-05-04 16:07:26 -0300

    hotfix: adição do amplify no cors

--- src/main/java/com/adote/api/infra/config/auth/SecurityConfig.java
+++ src/main/java/com/adote/api/infra/config/auth/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://adot-e.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://main.d99l6n0fah3sa.amplifyapp.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
